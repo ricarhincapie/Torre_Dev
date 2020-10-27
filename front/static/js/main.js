0f3f2f4dbaffd5f2c482c582b9cc2217b88bf8ec
Ricardo Hincapie <hincapiet.ricardo@gmail.com>
2020-10-27 01:05:47 -0500

    Modify main.css to include brand colours, main.js and index.html to fetch API and add new background and Readme images

--- front/static/js/main.js
+++ front/static/js/main.js
@@ -1,2 +1,26 @@
+const MY_URL = "http://127.0.0.1:7005/api/v1/"
 
+function userAction() {
+    let username = document.getElementById("search").value;
+    console.log(username);
+    let new_url = MY_URL + username;
+    getData(new_url)
+    .then(data => {
+    document.getElementById("demo").innerHTML = JSON.stringify(data);
+    console.log(data)
+    }) 
+};
+
+// Function to fetch data from a given URL
+async function getData(url) {
+    const response = await fetch(url, {
+      'mode': 'cors',
+      'method': 'GET',
+      'headers': {
+        'Origin': 'http://184.72.156.146'
+      }
+    });
+    const data = await response.json()
+    return data;
+  }
 
