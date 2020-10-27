c2f62edc2739574660e2453fb3cf976a637a6bbf
Ricardo Hincapie <hincapiet.ricardo@gmail.com>
2020-10-26 21:09:37 -0500

    Add API directory structure and initial files

---
+++ api/v1/app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Flask Application """
+from flask import Flask, render_template, make_response, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+app.register_blueprint(app_views)
+
+@app.errorhandler(404)
+def not_found(error):
+    """ 404 Error
+    ---
+    responses:
+      404:
+        description: a resource was not found
+    """
+    return make_response(jsonify({'error': "Not found"}), 404)
+
+
+if __name__ == "__main__":
+    """ Main Function """
+    host = '0.0.0.0'
+    port = '7005'
+    app.run(host=host, port=port, threaded=True, debug=False)
